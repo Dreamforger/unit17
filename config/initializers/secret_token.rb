27f71db13a6058ea4a39f6214b2b8d29ae3ee6fa
Dreamforger <matthew_gill@hotmail.co.uk>
2013-05-29 21:07:08 +0100

    Basic functionality

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unit17::Application.config.secret_token = '0ef160170a8090c75a37c066ace94f6ad93789215cebd07197811d9cf896dd10c32f0b0861a34fbd5e36c3ee317addae7fbf96bac3544935e812d01a3736d244'
